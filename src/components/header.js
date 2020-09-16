a376c9fdc9f24553539bc1891ed00cc7f180e02a
Nitish Kumar <krnitish@live.in>
2020-09-16 10:27:54 +0530

    animation

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,20 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import logo from '../images/logo-designcode.svg'
+import './header.css'
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header>
+    <div className="header">
+    <div className="headerGroup">
+      <Link to="/"><img alt="logo" src={logo} width="30"/></Link>
+      <Link to="/">Courses</Link>
+      <Link to="/">Downloads</Link>
+      <Link to="/">Workshops</Link>
+      <Link to="/"><button>Buy</button></Link>
     </div>
+  </div>
   </header>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,21 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <div>
+    <Layout/>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to<br /> Design and Code React app</h1>
+        <p>
+          Complete courses about tools and design system. Prototype and build
+          apps with React and Swift
+        </p>
+        <Link to="/page-2/">Watch the Video</Link>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  </div>
 )
 
 export default IndexPage
